c9ad2112ddee0e2f5ca8b8a22c1188e8b64ae18b
Muhamad Naufal Burhanuddin Balit <67968500+Muhamad-naufal@users.noreply.github.com>
2023-10-21 09:07:06 +0700

    update upload gambar

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { Pencil, PlusCircle, ImageIcon } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { Course } from "@prisma/client";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import { FileUpload } from "@/components/file-upload";
+
+interface ImageFormProps {
+    initialData: Course
+    courseId: string;
+};
+
+const formSchema = z.object({
+    imageUrl: z.string().min(1, {
+        message: "Foto Diperlukan",
+    }),
+});
+
+export const ImageForm = ({
+    initialData,
+    courseId
+}: ImageFormProps) => {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const toggleEdit = () => setIsEditing((current) => !current);
+
+    const router = useRouter();
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            await axios.patch(`/api/courses/${courseId}`, values);
+            toast.success("Kursus Berhasil Update");
+            toggleEdit();
+            router.refresh();
+        } catch {
+            toast.error("Ada yang salah nieh, coba lagi ya?");
+        }
+    }
+
+    return (
+        <div className="mt-6 border bg-slate-100 rounded-md p-4">
+            <div className="font-medium flex items-center justify-between">
+                Gambar Banner
+                <Button onClick={toggleEdit} variant="ghost">
+                    {isEditing && (
+                        <>Nggak Jadi</>
+                    )}
+                    {!isEditing && !initialData.imageUrl && (
+                        <>
+                            <PlusCircle className="h-4 w-4 mr-2" />
+                            Tambahkan Gambar
+                        </>
+                    )}
+                    {!isEditing && initialData.imageUrl && (
+                        <>
+                            <Pencil className="h-4 w-4 mr-2" />
+                            Edit Gambar
+                        </>
+                    )}
+                </Button>
+            </div>
+            {!isEditing && (
+                !initialData.imageUrl ? (
+                    <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
+                        <ImageIcon className="h-10 w-10 text-slate-500" />
+                    </div>
+                ) : (
+                    <div className="relative aspect-video mt-2">
+                        <Image
+                            alt="Upload"
+                            fill
+                            className="object-cover rounded-md"
+                            src={initialData.imageUrl}
+                        />
+                    </div>
+                )
+            )}
+            {isEditing && (
+                <div>
+                    <FileUpload
+                        endpoint="courseImage"
+                        onChange={(url) => {
+                            if (url) {
+                                onSubmit({ imageUrl: url });
+                            }
+                        }}
+                    />
+                    <div className="text-xs text-muted-foreground mt-4">
+                        16:9 aspect ratio recommended
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,6 +1,11 @@
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs";
+import { IconBadge } from "@/components/icon-badge";
+import { LayoutDashboard } from "lucide-react";
+import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
+import { ImageForm } from "./_components/image-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 	const { userId } = auth();
@@ -37,6 +42,17 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 					<span className="text-sm text-slate-700">
 						Kelengkapan Kursus {completionField}
 					</span>
+				</div>
+			</div>
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+				<div>
+					<div className="flex items-center gap-x-2">
+						<IconBadge icon={LayoutDashboard} />
+						<h2 className="text-xl">Buat Kursusmu Sendiri</h2>
+					</div>
+					<TitleForm initialData={course} courseId={course.id} />
+					<DescriptionForm initialData={course} courseId={course.id} />
+					<ImageForm initialData={course} courseId={course.id} />
 				</div>
 			</div>
 		</div>
